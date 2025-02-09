61a3003063edc3f14c9ebbec0b449a708c07a5e4
Matt Pitts <matt@gopitts.net>
2025-02-09 13:49:25 -0500

    tests

--- src/test/java/com/ninjaone/dundie_awards/service/GiveDundieAwardsServiceTest.java
+++ src/test/java/com/ninjaone/dundie_awards/service/GiveDundieAwardsServiceTest.java
@@ -1,12 +1,19 @@
 package com.ninjaone.dundie_awards.service;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,10 +23,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.ninjaone.dundie_awards.AwardsCache;
+import com.ninjaone.dundie_awards.config.TransactionManagementConfig;
 import com.ninjaone.dundie_awards.model.Employee;
 import com.ninjaone.dundie_awards.model.Organization;
 import com.ninjaone.dundie_awards.repository.ActivityRepository;
@@ -28,15 +39,14 @@ import com.ninjaone.dundie_awards.repository.OrganizationRepository;
 
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest
-// @ContextConfiguration(classes = TransactionManagementConfig.class)
-// @AutoConfigurationPackage(basePackages = "com.ninjaone.dundie_awards")
+@ContextConfiguration(classes = TransactionManagementConfig.class)
+@AutoConfigurationPackage(basePackages = "com.ninjaone.dundie_awards")
 class GiveDundieAwardsServiceTest {
+    @Mock
+    private PlatformTransactionManager mockTransactionManager;
 
     @Mock
     private EmployeeRepository mockEmployeeRepository;
-
-    @Autowired
-    private EmployeeRepository realEmployeeRepository;
 
     @Mock
     private ActivityRepository mockActivityRepository;
@@ -46,58 +56,63 @@ class GiveDundieAwardsServiceTest {
     @Mock
     private JmsTemplate mockJmsTemplate;
 
-    @InjectMocks
-    private GiveDundieAwardsService giveDundieAwardsService;
+    @Autowired
+    private PlatformTransactionManager realTransactionManager;
 
     @Autowired
-    private OrganizationRepository organizationRepository;
+    private EmployeeRepository realEmployeeRepository;
+
+    @Autowired
+    private OrganizationRepository realOrganizationRepository;
 
     private Organization organization1, organization2;
 
     @BeforeEach
     void setUp() {
-        organization1 = organizationRepository.save(Organization.builder().name("Some Organization").build());
-        organization2 = organizationRepository.save(Organization.builder().name("Another Organization").build());
+        organization1 = realOrganizationRepository.saveAndFlush(Organization.builder().name("Some Organization").build());
+        organization2 = realOrganizationRepository.saveAndFlush(Organization.builder().name("Another Organization").build());
         IntStream.rangeClosed(1, 10)
                 .mapToObj(i -> createTestEmployee(organization1, i))
-                .forEach(realEmployeeRepository::save);
+                .forEach(realEmployeeRepository::saveAndFlush);
 
         IntStream.rangeClosed(11, 15)
                 .mapToObj(i -> createTestEmployee(organization2, i))
-                .forEach(realEmployeeRepository::save);
+                .forEach(realEmployeeRepository::saveAndFlush);
     }
 
     @Test
     void giveDundieAwardsByOrganization_should_call_the_necessary_component_methods() {
-        Organization organization = new Organization();
+        Organization organization = Organization.builder().id(1L).name("Some Organization").build();
         int awardCount = 5;
         int expectedCount = 10;
-
-        when(mockEmployeeRepository.addDundieAwardsByOrganization(organization, awardCount)).thenReturn(expectedCount);
-
-        int result = giveDundieAwardsService.giveDundieAwardsByOrganization(organization, awardCount);
-
+        when(mockEmployeeRepository.addDundieAwardsByOrganization(eq(organization), eq(awardCount))).thenReturn(expectedCount);
+        GiveDundieAwardsService underTest = new GiveDundieAwardsService(mockTransactionManager, mockEmployeeRepository, mockActivityRepository, awardsCache, mockJmsTemplate);
+        Integer result = underTest.giveDundieAwardsByOrganization(organization, awardCount);
         assertEquals(expectedCount, result);
-        verify(mockEmployeeRepository, times(1)).addDundieAwardsByOrganization(organization, awardCount);
+        verify(mockEmployeeRepository, times(1)).addDundieAwardsByOrganization(eq(organization), eq(awardCount));
         verify(mockJmsTemplate, times(1)).convertAndSend(eq(GiveDundieAwardsService.DUNDIE_MESSAGES_QUEUE),
                 any(GiveDundieAwardsService.AwardsGivenMessage.class));
     }
 
-    // @Test
-    // void giveDundieAwardsByOrganization_should_rollback_database_transaction_when_message_submission_fails() {
-    //     // simulate an error when submitting the JMS message
-    //     doThrow(new RuntimeException("JMS error")).when(mockJmsTemplate).convertAndSend(eq(GiveDundieAwardsService.DUNDIE_MESSAGES_QUEUE),
-    //             any(GiveDundieAwardsService.AwardsGivenMessage.class));
-    //             GiveDundieAwardsService underTest = new GiveDundieAwardsService(realEmployeeRepository, mockActivityRepository, awardsCache, mockJmsTemplate);
-    //     Map<Employee, Integer> originalOrg1Counts = realEmployeeRepository.findByOrganization(organization1).stream()
-    //             .collect(Collectors.toMap(e -> e, Employee::getDundieAwards));
-    //     assertThat(originalOrg1Counts, aMapWithSize(10));
-    //     int awardCount = 5;
-    //     RuntimeException thrown = assertThrows(RuntimeException.class, () -> underTest.giveDundieAwardsByOrganization(organization1, awardCount));
-    //     Map<Employee, Integer> newOrg1Counts = realEmployeeRepository.findByOrganization(organization1).stream()
-    //             .collect(Collectors.toMap(e -> e, Employee::getDundieAwards));
-    //     assertThat(newOrg1Counts, is(originalOrg1Counts));
-    // }
+    @Test
+    void giveDundieAwardsByOrganization_should_rollback_database_transaction_when_message_submission_fails() {
+        // simulate an error when submitting the JMS message
+        doThrow(new RuntimeException("JMS error")).when(mockJmsTemplate).convertAndSend(eq(GiveDundieAwardsService.DUNDIE_MESSAGES_QUEUE),
+                any(GiveDundieAwardsService.AwardsGivenMessage.class));
+        GiveDundieAwardsService underTest = new GiveDundieAwardsService(realTransactionManager, realEmployeeRepository, mockActivityRepository, awardsCache, mockJmsTemplate);
+        Map<Employee, Integer> originalOrg1Counts = getEmployeeAwardCounts(organization1);
+        assertThat(originalOrg1Counts, aMapWithSize(10));
+        int awardCount = 5;
+        RuntimeException thrown = assertThrows(RuntimeException.class, () -> underTest.giveDundieAwardsByOrganization(organization1, awardCount));
+        assertThat(thrown.getMessage(), is("JMS error"));
+        Map<Employee, Integer> newOrg1Counts = getEmployeeAwardCounts(organization1);
+        assertThat(newOrg1Counts, is(originalOrg1Counts));
+    }
+
+    Map<Employee, Integer> getEmployeeAwardCounts(Organization organization) {
+        return realEmployeeRepository.findByOrganization(organization).stream()
+                .collect(Collectors.toMap(e -> e, Employee::getDundieAwards));
+    }
 
     static Employee createTestEmployee(Organization organization, int index) {
         return Employee.builder()
