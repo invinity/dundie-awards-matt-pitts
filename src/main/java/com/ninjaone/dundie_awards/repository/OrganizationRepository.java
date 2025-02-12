e145f75b5653662efe0d6d1002ce2bc8b9cf8e31
Matt Pitts <matt@gopitts.net>
2025-02-12 09:49:03 -0500

    Trying to get this to startup in ECS

--- src/main/java/com/ninjaone/dundie_awards/repository/OrganizationRepository.java
+++ src/main/java/com/ninjaone/dundie_awards/repository/OrganizationRepository.java
@@ -2,8 +2,10 @@ package com.ninjaone.dundie_awards.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import com.ninjaone.dundie_awards.model.Organization;
 
 @Repository
+@Transactional
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 }
